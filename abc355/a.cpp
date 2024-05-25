4b19c09297cf01a4ddd1f9a055166220d804f3d5
stream <yuhei.04184132@gmail.com>
2024-05-25 23:23:58 +0900

    2024/05/25 abc355-a

---
+++ abc355/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+using mint = modint998244353;
+#endif
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (ll i = s; i < n; i++)
+int main() {
+    ll a,b;
+    cin >> a >> b;
+    if(a==b){
+        cout << -1 << endl;
+        return 0;
+    }
+    cout << 6-a-b << endl;
+}
